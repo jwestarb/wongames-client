bd40bc26a36e56f13277572fd0a6a3a060e82105
Julio Cesar Westarb Jr <jwestarb@gmail.com>
2021-05-23 22:21:13 -0300

    Add GameCardSlider component

---
+++ src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,79 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import { GameCardProps } from 'components/GameCard'
+import GameCardSlider from '.'
+
+const items = [
+  {
+    id: '1',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 235,
+    promotionalPrice: 215
+  },
+  {
+    id: '2',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 235,
+    promotionalPrice: 215
+  },
+  {
+    id: '3',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 235,
+    promotionalPrice: 215
+  },
+  {
+    id: '4',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 235,
+    promotionalPrice: 215
+  },
+  {
+    id: '5',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 235,
+    promotionalPrice: 215
+  },
+  {
+    id: '6',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x145',
+    price: 235,
+    promotionalPrice: 215
+  }
+]
+
+export default {
+  title: 'GameCardSlider',
+  component: GameCardSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<GameCardProps[]> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <GameCardSlider items={args} {...args} />
+  </div>
+)
---
+++ src/components/GameCardSlider/test.tsx
@@ -0,0 +1,80 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+import GameCardSlider from '.'
+
+const items = [
+  {
+    id: '1',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: '235',
+    promotionalPrice: '215'
+  },
+  {
+    id: '2',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: '235',
+    promotionalPrice: '215'
+  },
+  {
+    id: '3',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: '235',
+    promotionalPrice: '215'
+  },
+  {
+    id: '4',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: '235',
+    promotionalPrice: '215'
+  },
+  {
+    id: '5',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: '235',
+    promotionalPrice: '215'
+  },
+  {
+    id: '6',
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x145',
+    price: '235',
+    promotionalPrice: '215'
+  }
+]
+
+describe('<GameCardSlider />', () => {
+  it('should render with 4 active items', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+  })
+
+  it('should render white arrows if color passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+})
