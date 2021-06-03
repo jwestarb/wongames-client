c197d92c63e071e31fe3bf999c4659e81759adf4
Julio Cesar Westarb Jr <jwestarb@gmail.com>
2021-06-03 20:17:57 -0300

    Styles in Auth template

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,5 +1,9 @@
 import Auth from 'templates/Auth'
 
 export default function SignIn() {
-  return <Auth />
+  return (
+    <Auth title="Sign in">
+      <h1>children sign in</h1>
+    </Auth>
+  )
 }
--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,8 +1,37 @@
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+
 import * as S from './styles'
 
-const Auth = () => (
+type AuthProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
-    <h1>Auth</h1>
+    <S.BannerBlock>
+      <S.BannerContent>
+        <Logo />
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
+        <S.Footer>WON Games 2020 © Todos os direitos reservados</S.Footer>
+      </S.BannerContent>
+    </S.BannerBlock>
+    <S.Content>
+      <S.ContentWrapper>
+        <Logo color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
+
+        {children}
+      </S.ContentWrapper>
+    </S.Content>
   </S.Wrapper>
 )
 
