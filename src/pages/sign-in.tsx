364279ad031fe0d36caf8bd9891fd2679b849e2a
Julio Cesar Westarb Jr <jwestarb@gmail.com>
2021-06-05 15:50:29 -0300

    Add links to menu and auth pages

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,9 +1,10 @@
+import FormSignIn from 'components/FormSignIn'
 import Auth from 'templates/Auth'
 
 export default function SignIn() {
   return (
     <Auth title="Sign in">
-      <h1>children sign in</h1>
+      <FormSignIn />
     </Auth>
   )
 }
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,9 +1,10 @@
+import FormSignUp from 'components/FormSignUp'
 import Auth from 'templates/Auth'
 
 export default function SignUp() {
   return (
     <Auth title="Sign up">
-      <h1>children sign up</h1>
+      <FormSignUp />
     </Auth>
   )
 }
